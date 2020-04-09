be0d3cf8409b9a4973771aaa8fed5e252661b5d2
Rahul Devadiga <43218543+RahulDevadiga@users.noreply.github.com>
2020-04-09 22:00:59 +0530

    New features and optimizations
    Clear all feature added which will clear the database.
    Error handling for invalid city name.
    Some minor code optimizations added.

--- app.py
+++ app.py
@@ -1,39 +1,58 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3 as sql 
 
 app = Flask(__name__)
 
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metrics&appid={}'
 key = "1a4c4f306c55a790989a33c6b9d37973"
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET','POST'])
 def index():
+	print('redirected to inedx')
+	error = None
 	db = sql.connect("cities.db")
 	cursor = db.cursor()	
 	if request.method == 'POST':
 		new_city = request.form.get('city')
-		cursor.execute("select * from city") 
-		cities = [ city[0] for city in cursor.fetchall() ]
-		if new_city and new_city not in cities :
-			cursor.execute(f"insert into city values('{new_city}')")
-			db.commit()
+		r = requests.get(url.format(new_city,key)).json()
+		if r['cod'] != '404':
+			cursor.execute("select * from city") 
+			cities = [ city[0] for city in cursor.fetchall() ]
+			if new_city and new_city not in cities :
+				cursor.execute(f"insert into city values('{new_city}')")
+				db.commit()
+		else:
+			error = "Such city name does not exists, Try again"
 	cursor.execute("select * from city")
 	cities = [ city[0] for city in cursor.fetchall() ]
+	print(cities)
 	cursor.close()
 	db.close()
-	url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}'
+	
 	weather_data = []
 	for city in cities:
 		r = requests.get(url.format(city,key)).json()
 		weather = {
             'city' : city,
-            'temperature' : r['main']['temp'],
+            'temperature' : round(r['main']['temp'] - 273.15,2),
             'description' : r['weather'][0]['description'],
             'icon' : r['weather'][0]['icon'],
         }
 		weather_data.append(weather)
-	return render_template('index.html', weather_data=weather_data)
+	print(weather_data)
+	return render_template('index.html', weather_data=weather_data, error = error)
 	
-	
+@app.route('/clear')
+def clear():
+	db = sql.connect("cities.db")
+	cursor = db.cursor()	
+	cursor.execute("delete from city")
+	db.commit()
+	cursor.close()
+	print("Successfully cleared")
+	db.close()
+	return redirect(url_for('index'))
 	
 if __name__ == "__main__" : 
 
